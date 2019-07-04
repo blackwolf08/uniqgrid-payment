ca5f7b883491c550ea141a509a3b1d51de8aefeb
Sunny Dhama <31814079+blackwolf08@users.noreply.github.com>
2019-07-04 13:48:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3005;
+const port = process.env.PORT || 3005;
 const apiRoute = require('./routes/api');
 const viewRoute = require('./routes/views');
 const bodyParser = require('body-parser');
