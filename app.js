4f43ff3230cbe06aacd0c154f8a9c18edb7c8bc1
Sunny Dhama <31814079+blackwolf08@users.noreply.github.com>
2019-07-05 00:19:17 +0530

    login

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('view engine', 'ejs');
 app.use(express.json());
 app.use('/static', express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
-  res.redirect('/views/payment-view');
+  res.redirect('/views/login');
 });
 
 app.use('/api', apiRoute);
